039fcabd7a7b8b5a878a7fdcb4f9b2f687540fef
psumiya <sumiya.p@gmail.com>
2023-02-04 22:19:59 -0800

    Add support for click-to-select-and-eval-expressions

--- jpath.js
+++ jpath.js
@@ -1,3 +1,17 @@
+const defaultJson = {"store":{"book":[{"category":"reference","author":"Nigel Rees","title":"Sayings of the Century","price":8.95},{"category":"fiction","author":"Evelyn Waugh","title":"Sword of Honour","price":12.99},{"category":"fiction","author":"Herman Melville","title":"Moby Dick","isbn":"0-553-21311-3","price":8.99},{"category":"fiction","author":"J. R. R. Tolkien","title":"The Lord of the Rings","isbn":"0-395-19395-8","price":22.99}],"bicycle":{"color":"red","price":19.95}},"expensive":10};
+const defaultExpression = "$..book[?(@.price <= $['expensive'])]";
+
+function reset() {
+    document.getElementById('jsonToEvaluate').value = JSON.stringify(defaultJson, null, '\t');
+    document.getElementById('pathExpression').value = defaultExpression;
+}
+
+function evaluateExpression(pathExpression) {
+    document.getElementById('jsonToEvaluate').value = JSON.stringify(defaultJson, null, '\t');
+    document.getElementById('pathExpression').value = pathExpression;
+    evaluateJson();
+}
+
 function beautify() {
     const jsonToEvaluate = document.getElementById('jsonToEvaluate').value;
     document.getElementById('jsonToEvaluate').value = getPrettyPrintString(jsonToEvaluate);
